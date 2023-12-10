1db17b564f5b9b663b0c6e2efe78b8724afef86e
stivemok <stivemok@gmail.com>
2023-12-10 21:14:26 +0300

    merge

--- easyCarRental/app/routes.py
+++ easyCarRental/app/routes.py
@@ -1,5 +1,15 @@
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Stive'}
+    return '''
+<html>
+    <header>
+        <title>Home page - Easy Rentals</title>
+    </header>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
